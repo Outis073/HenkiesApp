8762fadb2c990b169f0850e333ee7fdf2b533043
Xanh van Nguyen <xnguyen@dataleaf.nl>
2023-03-21 18:56:32 +0100

    test create

--- common/src/Logica/FileMethods.java
+++ common/src/Logica/FileMethods.java
@@ -2,12 +2,20 @@ package Logica;
 import java.io.*;
 import java.net.Socket;
 
+
+
 public class FileMethods implements FileLogica{
+
+
 
     @Override
     public  void create(String path, Socket socket)  throws Exception{
 
         try {
+
+            var dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+
             int bytes = 0;
             File file = new File(path);
             FileInputStream fileInputStream = null;
@@ -15,12 +23,12 @@ public class FileMethods implements FileLogica{
 
 
             // send file size
-            socket.getOutputStream().write((int) file.length());
+            dataOutputStream.writeLong(file.length());
             // break file into chunks
             byte[] buffer = new byte[4 * 1024];
             while ((bytes = fileInputStream.read(buffer)) != -1) {
-                socket.getOutputStream().write(buffer, 0, bytes);
-                socket.getOutputStream().flush();
+                dataOutputStream.write(buffer,0,bytes);
+                dataOutputStream.flush();
             }
             fileInputStream.close();
         } catch (IOException e) {
@@ -31,12 +39,14 @@ public class FileMethods implements FileLogica{
 
     @Override
     public void receiveFile(String fileName, Socket socket) throws Exception{
+        var dataInputStream = new DataInputStream(socket.getInputStream());
+
         int bytes = 0;
         FileOutputStream fileOutputStream = new FileOutputStream(fileName);
 
-        long size = socket.getInputStream().read();   // read file size
+        long size = dataInputStream.readLong();     // read file size
         byte[] buffer = new byte[4*1024];
-        while (size > 0 && (bytes = socket.getInputStream().read(buffer, 0, (int)Math.min(buffer.length, size))) != -1) {
+        while (size > 0 && (bytes = dataInputStream.read(buffer, 0, (int)Math.min(buffer.length, size))) != -1) {
             fileOutputStream.write(buffer,0,bytes);
             size -= bytes;      // read upto file size
         }
